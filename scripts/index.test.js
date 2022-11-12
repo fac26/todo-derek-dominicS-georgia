584095eec1f7ddce6bcba38cb5d002f50d361cba
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-11-12 22:03:19 +0000

    created a branch for testing

---
+++ scripts/index.test.js
@@ -0,0 +1,56 @@
+// add tasks to a list so that I can keep track of them
+
+test("Submitting a new task adds it to the list", () => {
+  // test goes here
+});
+
+// check things off my list so that I can see what I've done
+test("Checking an entry marks it as complete", () => {
+  // test goes here
+});
+
+// delete things from the list if I don't need to do them anymore
+test("Deleting an entry removes it from the list", () => {
+  // test goes here
+});
+
+///////////
+
+// DL tests
+
+test("does the tasks array have a new item in it after user clicks submit?", (event) => {
+  event.preventDefault();
+  // 1. grab the input element
+  const userInput = document.querySelector(".nameOfInputElement"); 
+  // 2. Enter a task into the input element
+  userInput.value = "Do this by Tuesday" 
+  // 3. grab the submit button
+  const submitBtn = document.querySelector(".nameOfSubmitBtn");
+  // 4. click the submit button to send the task off 
+  submitBtn.click(); 
+  // 5. check that the last item in the array has the same message
+  // (Or we could check the array length before submitting and compare it to after submitting?)
+  equal(tasksArray[tasksArray.length - 1], "Do this by Tuesday"); 
+  userInput.value = ""; //
+})
+
+
+test("has the DOM been updated after user clicks submit?", (event) => {
+  event.preventDefault();
+  // 1. grab the input element
+  const userInput = document.querySelector(".nameOfInputElement");
+  // 2. Enter a task into the input element
+  userInput.value = "Do this by Tuesday";
+  // 3. grab the submit button
+  const submitBtn = document.querySelector(".nameOfSubmitBtn");
+  // 4. click the submit button to send the task off
+  submitBtn.click();
+  // 5. grab the container where all the tasks are stored
+  const tasks = document.querySelector(".taskContainer");
+  // 6. check the last child in the taskcontainer has the most recent task
+  const mostRecentTask = tasks.lastElementChild;
+  equal(mostRecentTask.textContent, "Do this by Tuesday")
+  userInput.value = "";
+})
+
+/////
