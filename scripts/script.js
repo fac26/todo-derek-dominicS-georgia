16629bb49c780b521f264a718ea4d8da912d778a
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-11-15 13:30:54 +0000

    fixed blank task submission

--- scripts/script.js
+++ scripts/script.js
@@ -5,28 +5,31 @@ const list = document.querySelector(".list");
 // grabs submit button
 const submitBtn = document.getElementById("submit-btn");
 
+// add task functionality
+const addTaskItem = () => {
+  const newTask = toDo.value;
+
+  const taskItem = document.createElement("li");
+  taskItem.classList = "list";
+  if (newTask.trim() === "") {
+    return;
+  } else {
+    taskItem.innerHTML = `
+  <input aria-label="Mark task as complete" type="checkbox"/>
+  <span>${newTask}</span>
+  <button type="button" class="remove-task-btn"><i class="fa-solid fa-x"></i></button>
+  `;
+    list.appendChild(taskItem);
+    toDo.value = "";
+  }
+};
+
+// remove task button functionality
+
 // event listeners
 submitBtn.addEventListener("click", addTaskItem);
-toDo.addEventListener("keydown", (e) => {
+toDo.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     addTaskItem();
   }
 });
-
-// add task functionality
-const addTaskItem = () => {
-  const newTask = toDo.value;
-  toDo.value = "";
-
-  const taskItem = document.createElement("li");
-  // taskItem.classList = "";
-
-  taskItem.innerHTML =
-  `
-  <input aria-label="Mark task as complete" type="checkbox"/>
-  <span>${task}</span>
-  <button type="button" class="remove-task-btn"><i class="fa-solid fa-x"></i></button>
-  `
-  // remove task button? 
-  const removeTaskButton
-};
