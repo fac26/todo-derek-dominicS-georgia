a8769737bbc36ae8b2aa313313b88ec5a6319aeb
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-11-16 22:15:45 +0000

    inserted tests from testing branch

--- scripts/script.test.js
+++ scripts/script.test.js
@@ -1,38 +1,119 @@
-// minimum testing requirements
+// <======== TESTING THE ADD NEW TASK FUNCTIONALITY ========>
 
-//Add tasks to a list so that I can keep track of them
-test("Submitting a new task adds it to the list of to-dos", () => {
-  // grabs the list
-  const list = document.querySelector(".list");
-  // grabs submit button
-  const submitBtn = document.getElementById("submit-btn");
-  // create an artificial input for testing
-  const input = [
-    "Here's the first task",
-    "And another task",
-    "",
-    "Above this was a blank task",
-  ];
-  const inputArray = input.filter((e) => e);
-  inputArray.forEach((e) => {
-    input.value = e;
-    submitBtn.click();
-  });
-  const result = document.querySelectorAll("li");
-  const actual = result.length;
-  const expected = inputArray.length;
+test("Can a user add a new task to the list?", () => {
+  // 1. Create a new task
+  const input = document.querySelector("#to-do");
+  input.value = `This is a task`;
+  const submitButton = document.querySelector("#submit-btn");
+  submitButton.click();
 
+// 2. Grab all the li elements in the DOM to see how many tasks have been created
+const currentTasks = document.querySelectorAll(".listItem");
+
+// 3. Check the length of the list;
+NumOfTasks = currentTasks.length;
+
+// 4. Compare the number of tasks in the DOM with the expected number
+equal(NumOfTasks, 1);
+
+// 5. Reset DOM 
+const removeButtons = document.querySelectorAll(".remove-task-btn");
+removeButtons.forEach(button => button.click())
+})
+
+test("Testing whether the user's input is the task that's been added", () => {
+// 1. Create a new task
+const input = document.querySelector("#to-do");
+input.value = `This is a task`;
+const submitButton = document.querySelector("#submit-btn");
+submitButton.click();
+
+// 2. Grab all the li elements in the DOM to see how many tasks have been created
+const currentTask = document.querySelector(".listItem");
+
+// 3. Grab the task value;
+const taskToDo = currentTask.querySelector("span").textContent
+
+// 4. Compare the number of tasks in the DOM with the expected number
+equal(taskToDo, `This is a task`);
+
+// 5. Reset DOM 
+const removeButtons = document.querySelectorAll(".remove-task-btn");
+removeButtons.forEach(button => button.click())
+})
+
+test("Can a user add multiple new tasks to the list", () => {
+  // 1. Use a loop to create 3 different tasks
+  for (let i = 0; i < 3; i++){
+    const input = document.querySelector("#to-do");
+    input.value = `Task ${i + 1}: This is a task`;
+    const submitButton = document.querySelector("#submit-btn");
+    submitButton.click();
+  }
+  // 2. Grab all the li elements in the DOM to see how many tasks have been created
+  const currentTasks = document.querySelectorAll(".listItem");
+
+  // 3. Check the length of the list;
+  NumOfTasks = currentTasks.length;
+
+  // 4. Compare the number of tasks in the DOM with the expected number
+  equal(NumOfTasks, 3)
+
+  // 5. Reset DOM 
+  const removeButtons = document.querySelectorAll(".remove-task-btn");
+  removeButtons.forEach(button => button.click())
+})
+
+// <======== TESTING THE CHECKBOXES ========>
+
+test("Can a user mark a task as complete?", () => {
+  // 1. Select the input element and enter a new task
+  const input = document.querySelector("#to-do");
+  input.value = "Testing if checkboxes work";
+
+  // 2. Submit the task
+  const submitButton = document.querySelector("#submit-btn");
+  submitButton.click();
+
+  // 3. Select and then click the checkbox in the new task
+  const checkbox = document.querySelector("input[type='checkbox']");
+  checkbox.click();
+
+  // 4. Check whether the checkbox has been ticked and assign it to the variable `actual`
+  const checkboxStatus = checkbox.checked;
+  actual = checkboxStatus;
+
+  // 5. Set `expected` to true
+  expected = true;
+
+  // 6.  Compare actual and expected
   equal(actual, expected);
-  list.innerHTML = "";
+
+  // 7. Reset DOM 
+  const removeButtons = document.querySelectorAll(".remove-task-btn");
+  removeButtons.forEach(button => button.click())
 });
 
-// Check things off my list so that I can see what I’ve done
-test("Checking an entry marks it as complete", () => {
-  // test goes here
-});
+// <======== TESTING THE DELETE FUNCTION ========>
 
-// Delete things from the list if I don’t need to do them anymore
-test("Deleting an entry removes it from the list", () => {
-  // test goes here
-});
+test("Can the user delete a task from the list?", () => {
+  // 1. Create a new task
+  const input = document.querySelector("#to-do");
+  input.value = "Testing if the delete button works";
+  const submitButton = document.querySelector("#submit-btn");
+  submitButton.click();
 
+  // 2. Select and click the delete button
+  const deleteTaskButton = document.querySelector(".remove-task-btn");
+  deleteTaskButton.click();
+
+  // 3. Check how many tasks are on the page (should an empty nodelist)
+  const tasks = document.querySelectorAll(".listItem");
+  actual = tasks.length;
+
+  // 4. Compare actual and expected
+  equal(actual, 0)
+})
+
+
+// <======== TESTING THE LOCAL STORAGE FUNCTIONALITY ========>
